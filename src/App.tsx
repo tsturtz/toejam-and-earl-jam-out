f93f246ebd2a30a8800c744ddf6a9bf4bf066b95
Taylor Sturtz <taylor.sturtz@ytel.com>
2019-08-22 00:50:13 -0700

    got initial animations set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// import {funkotronicBeat} from './assets/funkotronic-beat.mp3';
 
 const App: React.FC = () => {
+
+  // Declare a new state variable, which we'll call "count"
+  const [shifty, setShifty] = useState(1);
+
+  useEffect (() => {
+    const interval = setInterval(() => {
+      // set background to a different sprite every 3 seconds
+      setShifty(Math.floor(Math.random() * 24) + 1);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []); // empty array here so this only runs once on component did mount (this seems weird 🤔)
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>[ 1337 TOEJAM AND EARL JAM OUT GAME HERE ]</h1>
+    <div id="body-bg">
+      <audio autoPlay controls>
+        <source src='./assets/funkotronic-beat.mp3' type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+      <div id="jam-out-bg" className={`shifty${shifty}`}>
+        <div id="toejam-box">
+          <div id="toejam" />
+        </div>
+        <div id="earl-box">
+          <div id="earl" />
+        </div>
+        <div id="jam-out-frame" />
+      </div>
+      <div id="jam-out-border" />
     </div>
   );
 }
